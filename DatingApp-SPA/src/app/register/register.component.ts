837bb4f7fa5db99a286c152e80d48818899f04ac
Christopher Clark <Chris@Christophers-MacBook-Pro.local>
2020-04-14 12:12:12 +0100

    End of Section 12

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_servcies/auth.service';
 import { createOfflineCompileUrlResolver } from '@angular/compiler';
 import { AlertifyService } from '../_servcies/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { BsDaterangepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_model/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,24 +14,58 @@ import { AlertifyService } from '../_servcies/alertify.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
+  registerForm: FormGroup;
+  bsConfig: Partial<BsDaterangepickerConfig>;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService,
+              private alertify: AlertifyService,
+              private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
+    this.bsConfig = {
+      containerClass: 'theme-red'
+    };
+    this.createRegisterForm();
   }
 
-  register(){
-    this.authService.register(this.model).subscribe(() => {
-      // console.log('registration sucessful');
-      this.alertify.success('Registration sucessful');
-    }, error => {
-      // console.log(error);
-      this.alertify.error(error);
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validators: this.passwordMatchValidator
     });
   }
 
-  cancel(){
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : { mismatch: true };
+  }
+
+  register() {
+    if  (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(() => {
+        this.alertify.success('Registration Successful');
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        this.authService.login(this.user).subscribe(() => {
+          this.router.navigate(['/members']);
+        });
+      });
+    }
+    console.log(this.registerForm.value);
+  }
+
+  cancel() {
     this.cancelRegister.emit(false);
     // console.log('cancelled');
   }
